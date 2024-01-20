039522b769d75263773c61b99cf73fabcbc2326c
Harshit Verma <harshitverma1996@yahoo.com>
2024-01-20 13:55:19 +0530

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -10,7 +10,7 @@ const Modal = ({ children, isOpen, onClose }) => {
       
       <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) =>
   e.stopPropagation()
-onClose();
+
 } >// This prevents the modal from closing when clicking inside
    //add code here
 {children}
